6bdc86a19dbd5eee726c5effb524145aca364076
tianchi <13058037784@163.com>
2020-11-12 17:19:10 +0800

    Update notice.py
    add sendtype pic、plot

--- ai_hub/notice.py
+++ ai_hub/notice.py
@@ -7,14 +7,22 @@ class notice:
     def sendmsg(self, msg):
         if not isinstance(msg,str):
             msg = str(msg)
-        if len(msg) >200:
-            msg = msg[:200]
+        if len(msg) >1024:
+            msg = msg[:1024]
 
         url = "http://35.194.206.106/notice"
         data = {"openid":self.openid,"msg":msg}
         print(data)
         res = requests.post(url=url,params=data)
         print(res.text)
+        
+    def sendpic(self, pic):
+        
+        print("send pic.")
+        
+    def sendplot(self, plot):
+        
+        print("send plot.")
 
     def setopenid(self, openid):
         self.openid = openid
